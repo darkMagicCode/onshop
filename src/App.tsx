5f6625e3eb79b74faae71a59beb757c4280c3085
ayman nawwar <darkmagiccode.com>
2024-05-02 12:23:40 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+import { TableDemo } from "./TableDemo";
+
+function App() {
+  return (
+    <>
+    <div className=" m-5">
+
+
+      <Button className=" mb-5">project with shadcn ui</Button>
+
+      <TableDemo />
+    </div>
+    </>
+  );
+}
+
+export default App;
